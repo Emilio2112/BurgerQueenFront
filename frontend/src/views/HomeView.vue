edd2ff4e4661853247bfc5ad85c862ee9c46782a
Joaquingse <joaquingse@gmail.com>
2022-12-09 11:32:59 +0000

    CREATE: - Add home page component - Add nav bar component

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,15 +1,22 @@
 <template>
-  <hello-world />
+  <v-app>
+    <v-col >
+      <v-row cols="12"> <nav-bar /></v-row>
+      <v-row> <hello-world /></v-row>
+    </v-col>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld.vue'
+import HelloWorld from "../components/HelloWorld.vue";
+import NavBar from "../components/NavBar.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
 
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    NavBar,
+  },
+};
 </script>
