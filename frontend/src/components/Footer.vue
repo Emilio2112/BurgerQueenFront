d37ba416177e277ea1dd5118b7d04cc3d022912e
Tommyscripts <dark.angel.alhue@gmail.com>
2022-12-10 14:38:47 +0000

    Fixed

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,45 +1,22 @@
 <template>
-    <v-footer
-      dark
-      padless
-      fixed         
-    >
-      <v-card
-        class="flex"
-        flat
-        tile
-      >
-        <v-card-title class="teal" >
-          
-  
-          <v-spacer></v-spacer>
-  
-          <v-btn
-            v-for="icon in icons"
-            :key="icon"
-            class="mx-2"
-            icon
-          >
-            <v-icon size="24px" color="#FFC300">
-              {{ icon }}
-            </v-icon>
-          </v-btn>
-        </v-card-title>
-  
-        
-        
-      </v-card>
-    </v-footer>
-  </template>
+  <v-footer dark padless fixed>
+    <v-card class="flex" flat tile>
+      <v-card-title class="teal">
+        <v-spacer></v-spacer>
+
+        <v-btn v-for="icon in icons" :key="icon" class="mx-2" icon>
+          <v-icon size="24px" color="#FFC300">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-title>
+    </v-card>
+  </v-footer>
+</template>
 <script>
-  export default {
-    data: () => ({
-      icons: [
-        'mdi-facebook',
-        'mdi-twitter',
-        'mdi-linkedin',
-        'mdi-instagram',
-      ],
-    }),
-  }
+export default {
+  data: () => ({
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+  }),
+};
 </script>
