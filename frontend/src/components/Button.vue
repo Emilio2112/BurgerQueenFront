5903f2c82d5dd3f41b04d8a5013c62d0fae90acd
Emilio <emilio_casado_de_galdo@hotmail.com>
2022-12-10 17:51:34 +0000

    Restaurant Card Button style

--- frontend/src/components/Button.vue
+++ frontend/src/components/Button.vue
@@ -1,20 +1,13 @@
 <template>
-  <v-btn
-    
-    elevation="2"
-    color="#001D3D"
-    class="red--text"
-    rounded
-    dark
-  >
-    <v-icon color="#FFC300"> mdi-arrow-left</v-icon>
+  <v-btn elevation="2" color="#001D3D" class="amber--text text--darken-1" rounded dark>
+    <v-icon color="#FFC300" class="mr-1"> mdi-arrow-left</v-icon>
     Volver
   </v-btn>
 </template>
 
 <script>
 export default {
-    name: "Button"
+  name: "Button",
 };
 </script>
 
