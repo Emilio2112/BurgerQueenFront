bbcf176eb51f38a965b4e6989a7827114fe08ea5
Joaquingse <joaquingse@gmail.com>
2022-12-11 13:43:20 +0000

    Updated: -Restaurant component Restaurant view

--- frontend/src/services/restaurant.js
+++ frontend/src/services/restaurant.js
@@ -1,7 +1,20 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'mongodb://localhost:27017'
+  baseURL: 'http://localhost:3000/api'
 })
 
 
+async function getRestaurants () {
+  try {
+    const response = await API.get('/restaurants')
+    return response.data
+  } catch (error) {
+    return { error: error.message }
+  }
+}
+
+
+export default {
+  getRestaurants
+}
