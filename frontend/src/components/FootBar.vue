e77a1e42e8c2d9bfb0c6e60fab6b17f749515ebe
Emilio <emilio_casado_de_galdo@hotmail.com>
2022-12-11 13:09:17 +0000

    icono footer

--- frontend/src/components/FootBar.vue
+++ frontend/src/components/FootBar.vue
@@ -3,7 +3,8 @@
   <v-footer dark padless fixed>
     <v-card class="flex" flat tile color="#001D3D">
       <v-card-title> 
-        <v-btn icon large outlined> <v-icon color="#FFC300"> mdi-email-outline</v-icon></v-btn>Contactanos
+        <v-btn icon large outlined> <v-icon color="#FFC300"> mdi-email-outline</v-icon></v-btn>
+        Contactanos
         <v-spacer></v-spacer>
         <v-btn v-for="icon in icons" :key="icon" class="mx-1" icon>
           <v-icon size="24px" color="#FFC300">
@@ -20,7 +21,6 @@
 export default {
   data: () => ({
     icons: ["mdi-facebook", "mdi-twitter", "mdi-instagram"],
-    icon: ["mdi-email"],
   }),
 };
 </script>
