eeca42efdb47581e59dc72fd9b2c714bf272942c
Joaquingse <joaquingse@gmail.com>
2022-12-13 09:24:20 +0000

    Updated due compring changes

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -1,35 +1,22 @@
 <template>
-<<<<<<< HEAD
 <div>
       <CreateRestaurant />
     <UpdateRestaurant />
 </div>
 
 
-=======
-  <div>
-    <CreateRestaurant />
-    <CreateBurger />
-  </div>
->>>>>>> 9eedb2aff4e973d27187a6d86f4b3f4918ae4e2c
 </template>
 
 <script>
 import CreateRestaurant from '@/components/CreateRestaurant.vue';
-<<<<<<< HEAD
 import UpdateRestaurant from '@/components/UpdateRestaurant.vue';
-=======
 import CreateBurger from '@/components/CreateBurger.vue';
->>>>>>> 9eedb2aff4e973d27187a6d86f4b3f4918ae4e2c
 
   export default {
    components:{
     CreateRestaurant,
-<<<<<<< HEAD
-    UpdateRestaurant
-=======
+    UpdateRestaurant,
     CreateBurger
->>>>>>> 9eedb2aff4e973d27187a6d86f4b3f4918ae4e2c
    } 
   }
 </script>
