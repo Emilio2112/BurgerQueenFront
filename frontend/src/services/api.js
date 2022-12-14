bcb8be269409dc384a915098104bcc0ed1b56d4c
Tommyscripts <dark.angel.alhue@gmail.com>
2022-12-14 09:53:44 +0000

    Render Conection

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import { useAuthStore } from '@/stores/store'
 
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://burguerqueen.onrender.com/api',
   headers: {token: localStorage.getItem('token')}
 })
 
