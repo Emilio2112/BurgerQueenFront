2b573fd844d382373f5b9bf068cb33ef07a61bfb
Joaquingse <joaquingse@gmail.com>
2022-12-15 10:58:32 +0000

    New component: -PopUp.vue Updated style components to get responsive.

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,54 +1,72 @@
 <template>
-  <v-card class="mt-10">
-    <v-text-field
-      label="Email"
-      placeholder="Email"
-      :rules="emailRules"
-      v-model="email"
-      filled
-      rounded
-      dense
-    ></v-text-field>
-    <v-text-field
-      label="Password"
-      :type="visible ? 'text' : 'password'"
-      :rules="passwordRules"
-      placeholder="Password"
-      v-model="password"
-      filled
-      rounded
-      dense
-      :append-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
-      @click:append="visible = !visible"
-    ></v-text-field>
-    <v-card-actions>
-      <v-btn
-        elevation="2"
-        color="#001D3D"
-        class="amber--text text--darken-1"
-        rounded
-        dark
-        @click="userLogin"
-      >
-        <v-icon color="#FFC300"> mdi-check </v-icon>
-        Aceptar
-      </v-btn>
-      <v-spacer></v-spacer>
+  <v-row>
+    <v-col cols="12" sm="10" md="6" class="mx-auto">
+      <v-card class="pa-4">
+        <v-card-title>Login</v-card-title>
+        <v-text-field
+          label="Email"
+          placeholder="Email"
+          :rules="emailRules"
+          v-model="email"
+          filled
+          rounded
+          dense
+        ></v-text-field>
+        <v-text-field
+          label="Password"
+          :type="visible ? 'text' : 'password'"
+          :rules="passwordRules"
+          placeholder="Password"
+          v-model="password"
+          filled
+          rounded
+          dense
+          :append-icon="visible ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="visible = !visible"
+        ></v-text-field>
+        <v-card-actions>
+          <v-btn
+            elevation="2"
+            color="#001D3D"
+            class="amber--text text--darken-1"
+            rounded
+            dark
+            @click="userLogin"
+          >
+            <v-icon color="#FFC300"> mdi-check </v-icon>
+            Aceptar
+          </v-btn>
+          <v-spacer></v-spacer>
 
-      <RouterLink :to="{ name: 'signup' }" style="text-decoration: none;">
-      <v-btn elevation="2" color="#001D3D" class="amber--text text--darken-1" rounded dark> <v-icon color="#FFC300"> mdi-account-outline </v-icon> 
-         Nueva Cuenta
-      </v-btn>
-    </RouterLink>
-    </v-card-actions>
-    <v-card-actions>
-      <v-btn elevation="2" color="#001D3D" class="amber--text text--darken-1" rounded dark @click="retroceder()">
-    <v-icon color="#FFC300" class="mr-1"> mdi-arrow-left</v-icon>
-    Volver
-  </v-btn>
-
-    </v-card-actions>
-  </v-card>
+          <RouterLink :to="{ name: 'signup' }" style="text-decoration: none">
+            <v-btn
+              elevation="2"
+              color="#001D3D"
+              class="amber--text text--darken-1"
+              rounded
+              dark
+            >
+              <v-icon color="#FFC300"> mdi-account-outline </v-icon>
+              Nueva Cuenta
+            </v-btn>
+          </RouterLink>
+        </v-card-actions>
+        <v-card-actions>
+          <v-btn
+            elevation="2"
+            color="#001D3D"
+            class="amber--text text--darken-1"
+            rounded
+            dark
+            @click="retroceder()"
+          >
+            <v-icon color="#FFC300" class="mr-1"> mdi-arrow-left</v-icon>
+            Volver
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -72,7 +90,7 @@ export default {
       email: "",
       password: "",
 
-      authStore: useAuthStore()
+      authStore: useAuthStore(),
     };
   },
   methods: {
@@ -88,15 +106,12 @@ export default {
         this.authStore.login(respond.token, respond.email);
         this.$router.push({ name: "home" });
       }
-
-     },
-     retroceder(){
-      this.$router.go(-1)
-  }
-},
-
-}
-
+    },
+    retroceder() {
+      this.$router.go(-1);
+    },
+  },
+};
 </script>
 
 <style scoped></style>
