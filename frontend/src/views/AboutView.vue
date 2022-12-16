ef2fdb156443ac8f57a957d7c17f3dbba3bd0b8c
Tommyscripts <dark.angel.alhue@gmail.com>
2022-12-16 10:08:55 +0000

    Updated: -About us

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -5,21 +5,23 @@
     class="mx-auto"
     max-width="700"
     outlined
+    color="#001D3D"
+   
   >
-    <v-list-item three-line>
-      <v-list-item-content>
+    <v-list-item three-line class="amber--text text--darken-1 ml-4">
+      <v-list-item-content >
         <div class="text-overline mb-4">
           Javier Cabrera
         </div>
 
-        <v-list-item-subtitle>Soy un desarrollador web Junior en proceso de aprendizaje.</v-list-item-subtitle>
+        <v-list-item-subtitle class="amber--text text--darken-1 ml-4">Soy un desarrollador web Junior en proceso de aprendizaje.</v-list-item-subtitle>
       </v-list-item-content>
 
       <v-list-item-avatar
         tile
         size="80"
         color="grey">
-        
+        <v-img  src="@/img/photo_2020-10-17_09-16-32.jpg"> </v-img>
     </v-list-item-avatar>
     </v-list-item>
 
@@ -28,15 +30,16 @@
         outlined
         rounded
         text
+        
       >
-        <a href="https://github.com/Tommyscripts">GitHub</a>
+        <a href="https://github.com/Tommyscripts" class="amber--text text--darken-1 ml-4">GitHub</a>
       </v-btn>
       <v-btn
         outlined
         rounded
         text
       >
-        <a href="https://www.linkedin.com/in/javierescoz/">Linkedin</a>
+        <a href="https://www.linkedin.com/in/javierescoz/" class="amber--text text--darken-1 ml-4">Linkedin</a>
       </v-btn>
     </v-card-actions>
   </v-card>
